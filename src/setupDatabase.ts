288e4b1595b836b9877a11910d41411364b821b6
Talitha <talithaalda.com>
2023-01-13 16:17:04 +0700

    setup project

---
+++ src/setupDatabase.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { config } from './config';
+import Logger from 'bunyan';
+
+const log: Logger = config.createLogger('SetupDatabase');
+export default () => {
+  const connect = () => {
+    mongoose.set('strictQuery', false);
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Successfully connected to database.');
+      })
+      .catch((error) => {
+        log.error('Error connecting to database', error);
+        return process.exit(1);
+      });
+  };
+  connect();
+  mongoose.connection.on('disconnected', connect);
+};
---
+++ src/setupServer.ts
@@ -0,0 +1,110 @@
+import { Application, json, urlencoded, Request, NextFunction, Response, request, response } from 'express';
+import http from 'http';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import 'express-async-errors';
+import compression from 'compression';
+import { config } from './config';
+import { createClient } from 'redis';
+import { Server } from 'socket.io';
+import applicationRoutes from './routes';
+import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
+import Logger from 'bunyan';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createAdapter } = require('@socket.io/redis-adapter');
+const SERVER_PQRT = 5000;
+const log: Logger = config.createLogger('server');
+
+export class ChattyServer {
+  private app: Application;
+  constructor(app: Application) {
+    this.app = app;
+  }
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standarMiddleware(this.app);
+    this.routeMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.startServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [config.SECRET_KEY_ONE!, config.SECRET_KEY_Two!],
+        maxAge: 24 * 7 * 3600000,
+        secure: config.NODE_ENV !== 'development'
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: config.CLIENT_URL,
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      })
+    );
+  }
+  private standarMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
+  private routeMiddleware(app: Application): void {
+    applicationRoutes(app);
+  }
+  private globalErrorHandler(app: Application): void {
+    app.all('*', (req = request, res = response) => {
+      res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found` });
+    });
+
+    app.use((error: IErrorResponse, _req: Request, res: Response, next: NextFunction) => {
+      log.error(error);
+      if (error instanceof CustomError) {
+        return res.status(error.statusCode).json(error.serializeErrors());
+      }
+      next();
+    });
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      const socketIO: Server = await this.createSocketIO(httpServer);
+      this.startHttpServer(httpServer);
+      this.socketIOConnections(socketIO);
+    } catch (error) {
+      log.error(error);
+    }
+  }
+  private async createSocketIO(httpServer: http.Server): Promise<Server> {
+    const io: Server = new Server(httpServer, {
+      cors: {
+        origin: config.CLIENT_URL,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      }
+    });
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    return io;
+  }
+
+  private startHttpServer(httpServer: http.Server): void {
+    log.info(`Server has started with process ${process.pid}`);
+    httpServer.listen(SERVER_PQRT, () => {
+      log.info(`Server running on port ${SERVER_PQRT}`);
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+  private socketIOConnections(io: Server): void {}
+}
